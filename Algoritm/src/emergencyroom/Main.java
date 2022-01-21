82b45f9fec78bd280ac65f0f7a3524ee519df5bf
takar <takartin@gmail.com>
2022-01-21 17:20:33 +0900

    선택정렬, 삽입정렬, 버블정렬

--- Algoritm/src/emergencyroom/Main.java
+++ Algoritm/src/emergencyroom/Main.java
@@ -2,21 +2,44 @@ package emergencyroom;
 
 import java.util.*;
 
+class Order{
+	int x, pt;
+	Order(int x, int pt) {
+		this.x = x;
+		this.pt = pt;
+	}
+}
+
 public class Main {
-	public int solution(int n, int m, int[] ep) {
-		int answer = 0;
-		Queue<Integer> Q = new LinkedList<>();
-		
+	public int solution(int n, int m, int[] er) {
+		int answer=0;
+		Queue<Order> Q = new LinkedList<>();
+		for(int i=0; i<n; i++) Q.offer(new Order(i, er[i]));
+		while(!Q.isEmpty()) {
+			Order tmp = Q.poll();
+			for(Order x : Q) {
+				if(x.pt > tmp.pt) {
+					Q.offer(tmp);
+					tmp = null;
+					break;
+				}
+			}
+			if(tmp!=null) {
+				answer++;
+				if(tmp.x==m) return answer;
+			}
+		}
+		return answer;
 	}
 	public static void main(String[] args) {
 		Main T = new Main();
 		Scanner scan = new Scanner(System.in);
 		int n = scan.nextInt();
 		int m = scan.nextInt();
-		int[] ep = new int[n];
+		int[] er = new int[n];
 		for(int i=0; i<n; i++) {
-			ep[i] = scan.nextInt();
+			er[i] = scan.nextInt();
 		}
-		System.out.print(T.solution(n, m, ep));
+		System.out.println(T.solution(n, m, er));
 	}
 }
---
+++ Algoritm/src/선택정렬/Main.java
@@ -0,0 +1,28 @@
+package 선택정렬;
+
+import java.util.*;
+
+public class Main {
+	public int[] solution(int n, int[] arr) {
+		for(int i=0; i<n-1; i++) { 
+			int idx = i;
+			for(int j=i+1; j<n; j++) {
+				if(arr[idx] > arr[j]) {
+					idx = j;
+				}
+			}
+			int tmp = arr[i];
+			arr[i] = arr[idx];
+			arr[idx] = tmp;
+		}
+		return arr;
+	}
+	public static void main(String[] args) {
+		Main T = new Main();
+		Scanner scan = new Scanner(System.in);
+		int n = scan.nextInt();
+		int[] arr = new int[n];
+		for(int i=0; i<n; i++) arr[i] = scan.nextInt();
+		for(int x : T.solution(n, arr)) System.out.print(x+ " ");
+	}
+}
