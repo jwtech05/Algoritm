df8bf97cccfba61d30a2a655311c3f0c3bc871e6
takar <takartin@gmail.com>
2022-03-03 14:29:14 +0900

    3/3-문자열/배열/효율성/스택큐

--- Algoritm2/src/스택큐/공주구하기/Main.java
+++ Algoritm2/src/스택큐/공주구하기/Main.java
@@ -1,5 +1,28 @@
 package 스택큐.공주구하기;
 
-public class Main {
+import java.util.*;
 
+public class Main {
+	public int solution(int n, int k) {
+		int answer = 0;
+		Queue<Integer> Q = new LinkedList<>();
+		for(int i=1; i<=n; i++) Q.offer(i);
+		int tmp = 0, a=0;
+		while(!Q.isEmpty()) {
+			tmp ++;
+			a = Q.poll();
+			if(tmp%k!=0) {
+				Q.offer(a);
+			}
+		}
+		answer = a;
+		return answer;
+	}
+	public static void main(String[] args) {
+		Main T = new Main();
+		Scanner scan = new Scanner(System.in);
+		int n = scan.nextInt();
+		int k = scan.nextInt();
+		System.out.print(T.solution(n, k));
+	}
 }
