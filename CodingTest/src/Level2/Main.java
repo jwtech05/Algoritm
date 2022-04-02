0cabb5f4501bd1f0f029d162365f25f85db069f6
takar <takartin@gmail.com>
2022-04-02 21:27:49 +0900

    기능개발 - queue/stream

--- CodingTest/src/Level2/Main.java
+++ CodingTest/src/Level2/Main.java
@@ -1,5 +1,27 @@
 package Level2;
 
-public class Main {
+import java.util.*;
+class Solution {
+    public int[] solution(int[] progresses, int[] speeds) {
+        Queue<Integer> queue = new LinkedList<>();
+        ArrayList<Integer> ans = new ArrayList<>();
+        for(int i=0; i<progresses.length; i++){
+            queue.offer((int) Math.ceil((100.0- progresses[i])/speeds[i]));
+        }
+        while(!queue.isEmpty()){
+            int day = queue.poll();
+            int cnt = 1;
 
+            while(!queue.isEmpty() && day >= queue.peek()){
+                cnt ++;
+                queue.poll();
+            }
+            ans.add(cnt);
+        }
+        int[] answer = new int[ans.size()];
+        for(int i=0; i<ans.size(); i++){
+            answer[i] = ans.get(i);
+        }
+        return answer;
+    }
 }
