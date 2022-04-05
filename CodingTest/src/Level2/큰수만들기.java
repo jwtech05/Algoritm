c1f6f6826aab33535ed263e35cdb1833c63dc3df
takar <takartin@gmail.com>
2022-04-05 22:28:30 +0900

    그리디, 동적계획법

---
+++ CodingTest/src/Level2/큰수만들기.java
@@ -0,0 +1,25 @@
+package Level2;
+
+public class 큰수만들기 {
+	class Solution {
+	    public String solution(String number, int k) {
+	        String answer = "";
+	        StringBuilder sb = new StringBuilder();
+	        int index = 0;
+	        int max = 0;
+	        for(int i=0; i<number.length()-k; i++){
+	            max = 0;
+	            for(int j= index; j<=k+i; j++){
+	                if(max < number.charAt(j)-'0'){
+	                    max = number.charAt(j)- '0';
+	                    index = j+1;
+	                }
+	                
+	            }
+	            sb.append(max);
+	        }
+	        answer = sb.toString();
+	        return answer;
+	    }
+	}
+}
